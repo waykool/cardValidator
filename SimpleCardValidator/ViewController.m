706f9cde374e376216d7903807af4c7aa54a55ff
Pedro Peres <pedro@happyinspector.com>
2015-08-09 21:01:10 -0700

    Added UI components

--- SimpleCardValidator/ViewController.m
+++ SimpleCardValidator/ViewController.m
@@ -12,16 +12,56 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UITextField *_textfield;
+    UILabel *_creditCardLabel;
+    UIButton *_button;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// MARK - Settup
+
+- (void) setupView {
+    CGRect labelFrame = CGRectMake(10, 20, 120, 45);
+    CGFloat textFieldXCoor = labelFrame.size.width + 10;
+    CGFloat textFieldWidth = self.view.frame.size.width - textFieldXCoor - 10;
+    CGRect textFieldFrame = CGRectMake(textFieldXCoor, 20, textFieldWidth, 45);
+    CGRect buttonFrame = CGRectMake(20, 70, 100, 45);
+    CGRect creditCardLabelFrame = CGRectMake(140, 70, 100, 45);
+
+    UILabel *label = [[UILabel alloc] initWithFrame:labelFrame];
+    label.text = @"Card Number: ";
+    [self.view addSubview:label];
+    label.textColor = [UIColor whiteColor];
+
+    _textfield = [[UITextField alloc] initWithFrame:textFieldFrame];
+    _textfield.placeholder = @"ie. 4217-6989-2345-2345";
+    _textfield.backgroundColor = [UIColor lightGrayColor];
+    _textfield.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin;
+    [self.view addSubview:_textfield];
+
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    [_button setTitle:@"Submit" forState:UIControlStateNormal];
+    [_button addTarget:self action:@selector(buttonTappedAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_button];
+
+    _creditCardLabel = [[UILabel alloc] initWithFrame:creditCardLabelFrame];
+    label.text = @"Card Number: ";
+    [self.view addSubview:label];
+    label.textColor = [UIColor whiteColor];
+}
+
+- (void) buttonTappedAction {
+
 }
 
 @end
