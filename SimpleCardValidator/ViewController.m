b0bbdb29fed7970fe36472dfc50c5a3f86b25568
Pedro Peres <pedro@happyinspector.com>
2015-08-09 21:32:18 -0700

    implemented Luhn alg

--- SimpleCardValidator/ViewController.m
+++ SimpleCardValidator/ViewController.m
@@ -47,6 +47,7 @@
     _textfield.placeholder = @"ie. 4217-6989-2345-2345";
     _textfield.backgroundColor = [UIColor lightGrayColor];
     _textfield.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin;
+    _textfield.delegate = self;
     [self.view addSubview:_textfield];
 
     _button = [[UIButton alloc] initWithFrame:buttonFrame];
@@ -60,8 +61,96 @@
     label.textColor = [UIColor whiteColor];
 }
 
-- (void) buttonTappedAction {
+// MARK - Actions
 
+- (void) buttonTappedAction {
+    BOOL isCardValid = [self validateCard];
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@""
+                                                                   message: [self alertMessage:isCardValid]
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction * action) {}];
+
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+// MARK - UITextFieldDelegate methods
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if([self isAllDigits: string]){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// MARK - Service Mehtods
+
+- (BOOL) isAllDigits:(NSString *) string {
+    NSCharacterSet* nonNumbers = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    NSRange r = [string rangeOfCharacterFromSet: nonNumbers];
+    return r.location == NSNotFound;
+}
+
+-(NSString *) alertMessage:(BOOL) isCardValid {
+    if (isCardValid) {
+        return @"Card number is valid!";
+
+    } else {
+        return @"Card number is not valid!";
+    }
+}
+
+/*
+ calculating doubles for the Luhn algorithm
+ - if result has only one digit we leave it alone
+ - if result has 2 digits we add the digits
+ - ie. if double = 12 then result = 3
+
+ - in order not to have to split numbers we apply
+ the formula d = 1 + (2*(x-5)) for numbers greater than 5.
+ */
+- (BOOL) validateCard {
+    NSArray *numberArray = [self getArrayOfNumbersFromString: _textfield.text];
+    NSInteger total = 0;
+
+    for(int i = 0; i < numberArray.count; i++){
+        NSInteger currentNumber = [numberArray[i] integerValue];
+
+        if((i % 2) != 0) {
+            if(currentNumber < 5) {
+                total += 2 * currentNumber;
+
+            } else {
+                total += 1 + (2 * (currentNumber - 5));
+            }
+        } else {
+            total += currentNumber;
+        }
+    }
+
+    return ((total%10) == 0);
+}
+
+/*
+ take a string and iterate through chrarcters
+ if character is a digit add it to the array
+ */
+- (NSArray *) getArrayOfNumbersFromString:(NSString *) string {
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    NSString *cardNumberString = _textfield.text;
+    NSString *numberString;
+
+    for(int i = (int) (cardNumberString.length - 1); i >= 0; i--){
+        numberString = [NSString stringWithFormat:@"%C", [cardNumberString characterAtIndex:i]];
+        NSInteger integerValue = [numberString integerValue];
+        [numberArray addObject:[[NSNumber alloc] initWithInteger: integerValue]];
+    }
+    
+    return numberArray;
 }
 
 @end
