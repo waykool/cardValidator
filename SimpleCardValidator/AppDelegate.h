24fbdc882d35ac9f7ac060bcd1bd60ae4f1e64a7
Pedro Peres <pedro@happyinspector.com>
2015-08-09 20:16:54 -0700

    Initial Commit

---
+++ SimpleCardValidator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleCardValidator
+//
+//  Created by Pedro Peres on 8/9/15.
+//  Copyright (c) 2015 Pedro Peres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleCardValidator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleCardValidator
+//
+//  Created by Pedro Peres on 8/9/15.
+//  Copyright (c) 2015 Pedro Peres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
